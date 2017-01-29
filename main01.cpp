af35d3ca794c8bc0d8884b78a483863892e2244e
Rodney <reln13st@gmail.com>
2017-01-29 01:56:56 +0100

    final model commented

--- main01.cpp
+++ main01.cpp
@@ -16,9 +16,10 @@
 using namespace cv;
 using namespace std;
 
+// Global variables
 Mat imagen, screenBuffer;
-int radius, last_x, last_y, new_size;
-bool drawing;
+int radius, last_x, last_y, new_size, n_classes;
+bool drawing, fnt, hnd, img;
 const int NUMBERS = 0;
 const int UPPERS = 10;
 const int LOWERS = 36;
@@ -38,7 +39,8 @@ string fill_zeros(int num, int num_zeros)
 	return ret;
 }
 
-void preprocess(Mat image, Mat &row_data, bool debugP = false, int c=0) {
+// preprocess image
+void preprocess(Mat image, Mat &row_data, int c=0) {
     Mat binary, resized, result, vect;
     int black_dots = 0;
 
@@ -83,21 +85,12 @@ void preprocess(Mat image, Mat &row_data, bool debugP = false, int c=0) {
         result = resized;
     }
 
-    /*if(c%20==0){
-        namedWindow("source", WINDOW_NORMAL);
-        namedWindow("resized", WINDOW_NORMAL);
-        namedWindow("result", WINDOW_NORMAL);
-        imshow("source", image);
-        imshow("resized", resized);
-        imshow("result", result);
-        waitKey();
-    }*/
-
     result.convertTo(vect, CV_32F);
     row_data = vect.reshape(1,1);
     divide(row_data, 255, row_data);
 }
 
+// read a folder
 int read_folder(string folder, vector<Mat> &data) {
     string name;
     Mat image, row_data;
@@ -110,7 +103,7 @@ int read_folder(string folder, vector<Mat> &data) {
             if (name.find(".png")!=string::npos){
                 image = imread(folder+name, IMREAD_GRAYSCALE);
                 if(image.data) {
-                    preprocess(image, row_data, false, c);
+                    preprocess(image, row_data, c);
                     data.push_back(row_data);
                     c++;
                 }
@@ -123,34 +116,39 @@ int read_folder(string folder, vector<Mat> &data) {
     return c;
 }
 
+// function to call read folder for each type of images
 void load_and_preprocess(string path, int n_folder, vector<Mat> &data, int &num_samples){
     string folder;
     int c = 0;
 
-    folder = path+"Fnt/Sample"+fill_zeros(n_folder+1, 3)+"/";
-    c += read_folder(folder, data);
-
-    folder = path+"Hnd/Sample"+fill_zeros(n_folder+1, 3)+"/";
-//    c += read_folder(folder, data);
-
-    folder = path+"Img/Sample"+fill_zeros(n_folder+1, 3)+"/";
-//    c += read_folder(folder, data);
+    if(fnt){
+        folder = path+"Fnt/Sample"+fill_zeros(n_folder+1, 3)+"/";
+        c += read_folder(folder, data);
+    }
+    if(hnd){
+        folder = path+"Hnd/Sample"+fill_zeros(n_folder+1, 3)+"/";
+        c += read_folder(folder, data);
+    }
+    if(img){
+        folder = path+"Img/Sample"+fill_zeros(n_folder+1, 3)+"/";
+        c += read_folder(folder, data);
+    }
 
     num_samples = c;
 }
 
-// reads data and stores in trainX, trainY, testX, testY
-void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, int num_classes, float split, int n_threads = 8){
+// read data and stores in trainX, trainY, testX, testY
+void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, float split, int n_threads = 8){
     int train_samples, test_samples;
     vector<thread> threads(n_threads);
-    vector<vector<Mat>> data(num_classes, vector<Mat>());
-    vector<int> num_samples(num_classes);
+    vector<vector<Mat>> data(n_classes, vector<Mat>());
+    vector<int> num_samples(n_classes);
 
     cout<<"Loading images and preprocessing..."<<endl;
     int i = 0, c = 0;
-    while (i<num_classes) {
+    while (i<n_classes) {
         c = 0;
-        for(int j=0; j<n_threads && i<num_classes; j++) {
+        for(int j=0; j<n_threads && i<n_classes; j++) {
             threads[j] = thread(load_and_preprocess, path, i+from_class, ref(data[i]), ref(num_samples[i]));
             i++;
             c++;
@@ -159,12 +157,6 @@ void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, in
             threads[j].join();
         }
     }
-//    load_and_preprocess(path, i+from_class, ref(data[i]), ref(num_samples[i]));
-//    i++;
-//    load_and_preprocess(path, i+from_class, ref(data[i]), ref(num_samples[i]));
-//    i++;
-//    load_and_preprocess(path, i+from_class, ref(data[i]), ref(num_samples[i]));
-//    waitKey();
 
     cout<<"Preparing data..."<<endl;
     Mat trainData;
@@ -172,7 +164,7 @@ void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, in
     Mat testData;
 	Mat testClasses;
 	cout<<"Training samples: ";
-    for(int i = 0; i < num_classes; i++) {
+    for(int i = 0; i < n_classes; i++) {
         train_samples = (int)round(num_samples[i]*split);
         cout<<train_samples<<", ";
         for(int j=0; j<num_samples[i]; j++){
@@ -188,7 +180,7 @@ void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, in
     }
     cout<<"= "<<trainData.rows<<endl;
     cout<<"Testing samples: ";
-    for(int i = 0; i < num_classes; i++) {
+    for(int i = 0; i < n_classes; i++) {
         cout<<num_samples[i] - (int)round(num_samples[i]*split)<<", ";
     }
     cout<<"= "<<testData.rows<<endl;
@@ -198,45 +190,25 @@ void read_data(string path, Mat &trainX, Mat &testX, Mat &trainY, Mat &testY, in
     testY = testClasses;
 }
 
+// train the SVM model
 Ptr<ml::SVM> trainSVM(Mat X, Mat Y, bool save=false) {
     Y.convertTo(Y,CV_32S);
     Ptr<ml::SVM> svm = ml::SVM::create();
     svm->setType(ml::SVM::C_SVC);
     svm->setKernel(ml::SVM::LINEAR);
-//    svm->setC(0.08);
     svm->setC(0.1);
-//    svm->setC(2.6);
-//    svm->setGamma(5.4);
-//    svm->setC(12.5);
-//    svm->setGamma(0.00225);
     svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 1e3, 1e-6));
     Ptr<ml::TrainData> td = ml::TrainData::create(X, ml::ROW_SAMPLE, Y);
     svm->train(td);
-//    ml::ParamGrid gridC(0.1,1.6,2);
-//    ml::ParamGrid gridGamma(1e-5,100,5);
-//    svm->trainAuto(td, 10, gridC);
-//    cout<<to_string(svm->getC())<<endl;
-//    cout<<to_string(svm->getGamma())<<endl;
-//    cout<<to_string(svm->getP())<<endl;
-//    cout<<to_string(svm->getNu())<<endl;
-//    cout<<to_string(svm->getCoef0())<<endl;
-//    cout<<to_string(svm->getDegree())<<endl;
 
     if (save){
-        svm->save("svm.yml");
+        svm->save("svm"+to_string(n_classes)+".yml");
     }
     return svm;
 }
 
-Ptr<ml::KNearest> trainKNN(Mat X, Mat Y){
-    Ptr<ml::KNearest> knn = ml::KNearest::create();
-    knn->train(X, ml::ROW_SAMPLE, Y);
-    return knn;
-}
-
-// evaluates the prediction
+// evaluate the prediction
 float evaluate(cv::Mat& predicted, cv::Mat& actual) {
-//    cout<<predicted.rows<<" - "<<actual.rows<<endl;
     assert(predicted.rows == actual.rows);
     int t = 0;
     int f = 0;
@@ -252,6 +224,7 @@ float evaluate(cv::Mat& predicted, cv::Mat& actual) {
     return (t * 1.0) / (t + f);
 }
 
+// get the label string
 string get_label(int n) {
     char letters[] = "abcdefghijklmnopqrstuvwxyz";
     if (from_class == NUMBERS) {
@@ -265,35 +238,14 @@ string get_label(int n) {
     }
 }
 
+// predict with the model
 void predictSVM(Ptr<ml::SVM> model, Mat X, Mat Y) {
-//    cout<<Y.rows<<endl;
-    for(int i = 0; i < 9000 && i < Y.rows; i+=30) {
-        Mat res;
-        Mat sample = X.row(i);
-        model->predict(sample, res);
-        string actu = get_label(Y.at<float>(i));
-        string pred = get_label(res.at<float>(0));
-        cout << actu << ":" << pred << "\t";
-        /*if (actu != pred) {
-            Mat reshaped = X.row(i).reshape(1,new_size);
-            reshaped.convertTo(reshaped, 0);
-            imshow("test"+to_string(i), reshaped);
-        }*/
-    }
-    cout<<endl;
-
     Mat predicted(Y.rows, 1, CV_32F);
     model->predict(X, predicted);
     cout << "Accuracy = " << evaluate(predicted, Y) << endl;
 }
 
-void predictKNN(Ptr<ml::KNearest> model, Mat X, Mat Y) {
-    Mat predicted(Y.rows, 1, CV_32F);
-    model->findNearest(X, 6, predicted);
-    cout << "Accuracy = " << evaluate(predicted, Y) << endl;
-}
-
-// calculates the elapsed time
+// calculate the elapsed time
 string elapsed_time(std::chrono::steady_clock::time_point start_time, std::chrono::steady_clock::time_point end_time) {
     float elapsed = (std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count()) / 1000000.0;
     if (elapsed > 60) {
@@ -304,6 +256,7 @@ string elapsed_time(std::chrono::steady_clock::time_point start_time, std::chron
     }
 }
 
+// classify one sample
 void classify(Ptr<ml::SVM> model, Mat sample) {
     Mat res, row_data;
     preprocess(sample, row_data, false);
@@ -311,80 +264,76 @@ void classify(Ptr<ml::SVM> model, Mat sample) {
     cout<<get_label(res.at<float>(0))<<endl;
 }
 
-Ptr<ml::SVM> get_model(bool train) {
-//Ptr<ml::KNearest> get_model(bool train) {
+// train the model
+Ptr<ml::SVM> get_model(string path_data, float split, int n_threads=1) {
 
     Ptr<ml::SVM> model;
-//    Ptr<ml::KNearest> model;
-    int n_classes = 10;
-    float split = 0.8;
-    int n_threads = 8;
 
-    if (train) {
-        string path_data = "data/";
-        std::chrono::steady_clock::time_point start_time;
-        std::chrono::steady_clock::time_point end_time;
-        Mat trainX, testX, trainY, testY;
+    std::chrono::steady_clock::time_point start_time;
+    std::chrono::steady_clock::time_point end_time;
+    Mat trainX, testX, trainY, testY;
 
-        // Reading
-        cout << "\nStart reading:\n";
+    // Reading
+    cout << "\nStart reading:\n";
 
-        start_time = std::chrono::steady_clock::now();
-        read_data(path_data, trainX, testX, trainY, testY, n_classes, split, n_threads);
-        cout<<trainX.size()<<endl;
-        cout<<testX.size()<<endl;
-        end_time = std::chrono::steady_clock::now();
+    start_time = std::chrono::steady_clock::now();
+    read_data(path_data, trainX, testX, trainY, testY, split, n_threads);
+    cout<<trainX.size()<<endl;
+    cout<<testX.size()<<endl;
+    end_time = std::chrono::steady_clock::now();
 
-        cout << "Reading completed in " << elapsed_time(start_time, end_time) << endl;
+    cout << "Reading completed in " << elapsed_time(start_time, end_time) << endl;
 
 
-        // Training
-        cout << "\nStart training:\n";
-        start_time = std::chrono::steady_clock::now();
-        model = trainSVM(trainX, trainY);
-//        model = trainKNN(trainX, trainY);
-        end_time = std::chrono::steady_clock::now();
-        cout << "Training completed in " << elapsed_time(start_time, end_time) << endl;
+    // Training
+    cout << "\nStart training:\n";
+    start_time = std::chrono::steady_clock::now();
+    model = trainSVM(trainX, trainY);
+    end_time = std::chrono::steady_clock::now();
+    cout << "Training completed in " << elapsed_time(start_time, end_time) << endl;
 
 
-        // Predicting
-        cout << "\nStart predicting:\n";
-        start_time = std::chrono::steady_clock::now();
-        predictSVM(model, testX, testY);
-//        predictKNN(model, testX, testY);
-        end_time = std::chrono::steady_clock::now();
-        cout << "Predicting completed in " << elapsed_time(start_time, end_time) << endl;
+    // Predicting
+    cout << "\nStart predicting:\n";
+    start_time = std::chrono::steady_clock::now();
+    predictSVM(model, testX, testY);
+    end_time = std::chrono::steady_clock::now();
+    cout << "Predicting completed in " << elapsed_time(start_time, end_time) << endl;
 
 
-        // Training and saving final model
-        cout << "\nStart training and saving final model:\n";
-        start_time = std::chrono::steady_clock::now();
-        Mat completeX, completeY;
-        vconcat(trainX, testX, completeX);
-        vconcat(trainY, testY, completeY);
-        model = trainSVM(completeX, completeY, true);
-//        model = trainKNN(trainX, trainY);
-        end_time = std::chrono::steady_clock::now();
-        cout << "Training and saving final model completed in " << elapsed_time(start_time, end_time) << endl;
-    }
-    else {
-        model = ml::StatModel::load<ml::SVM>("svm.yml");
-    }
+    // Training and saving final model
+    cout << "\nStart training and saving final model:\n";
+    start_time = std::chrono::steady_clock::now();
+    Mat completeX, completeY;
+    vconcat(trainX, testX, completeX);
+    vconcat(trainY, testY, completeY);
+    model = trainSVM(completeX, completeY, true);
+    end_time = std::chrono::steady_clock::now();
+    cout << "Training and saving final model completed in " << elapsed_time(start_time, end_time) << endl;
 
     return model;
 }
 
+// load the model
+Ptr<ml::SVM> get_model() {
+    Ptr<ml::SVM> model = ml::StatModel::load<ml::SVM>("svm"+to_string(n_classes)+".yml");
+    return model;
+}
+
+// draw a circle where the cursor is
 void draw_cursor(int x, int y){
 	screenBuffer=imagen.clone();
 	circle(screenBuffer, Point(x,y), radius, CV_RGB(0,0,0), -1, CV_AA);
 }
 
+// draw a circle in the drawing window
 void draw(int x,int y){
 	circle(imagen, Point(x,y), radius, CV_RGB(0,0,0), -1, CV_AA);
 	screenBuffer = imagen.clone();
 	imshow("Drawing Window", screenBuffer);
 }
 
+// on mouse events
 void on_mouse(int event, int x, int y, int flags, void* param) {
 	last_x=x;
 	last_y=y;
@@ -403,25 +352,52 @@ void on_mouse(int event, int x, int y, int flags, void* param) {
     }
 }
 
+// help
+void help(){
+    cout<<"Use the mouse to draw a character"<<endl;
+    cout<<"Press 'r' to classify"<<endl;
+    cout<<"Press 'e' to reset"<<endl;
+    cout<<"Press '+' to increase the radius"<<endl;
+    cout<<"Press '-' to decrease the radius"<<endl;
+    cout<<"Press 't' to save the current image"<<endl;
+    cout<<"Press 'ESC' to quit"<<endl;
+    cout<<"-----------------------------------"<<endl<<endl;
+}
+
+// main
 int main()
 {
-    cout << "CHARS74K\n\n";
+    // configurable variables
+    string path_data = "data/"; // relative path to images
+    new_size = 64; // size to resize
+    n_classes = 10; // number of classes
+    float split = 0.8; // percentage to split in training and testing
+    int n_threads = 8; // number of cores to use
+    fnt = true; // use font type images
+    hnd = false; // use hand type images
+    img = false; // use image type images
 
+    // private variables
 	int key;
 	bool execute = true;
-	bool train = false;
     drawing = false;
-    new_size = 16;
-	radius = 6;
+	radius = 7;
 	last_x=last_y=0;
 	imagen= Mat(Size(128,128),CV_8U, 255);
 	screenBuffer=imagen.clone();
 
-	Ptr<ml::SVM> model = get_model(train);
-//	Ptr<ml::KNearest> model = get_model(train);
+    cout << "Character Recognition\n\n";
 
+    // train the model
+//	Ptr<ml::SVM> model = get_model(path_data, split, n_threads);
+    // load the model
+    Ptr<ml::SVM> model = get_model();
+
+    // drawing window
 	namedWindow("Drawing Window", CV_WINDOW_FREERATIO);
 	setMouseCallback("Drawing Window", &on_mouse);
+
+	help();
 
     while(execute){
 
@@ -429,29 +405,29 @@ int main()
         key = waitKey(10);
 
         switch ((char)key){
-            case 27:
+            case 27: // ESC, finish the program
                 execute = false;
                 break;
-            case '+':
+            case '+': // increase the radius
             case 'w':
                 radius++;
                 draw_cursor(last_x, last_y);
                 break;
-            case '-':
+            case '-': // decrease the radius
             case 'q':
                 if (radius>1) {
                     radius--;
             		draw_cursor(last_x, last_y);
                 }
                 break;
-            case 'e':
+            case 'e': // reset the canvas
                 imagen = 255;
                 draw_cursor(last_x, last_y);
                 break;
-            case 't':
+            case 't': // save the image
                 imwrite("output.png", imagen);
                 break;
-            case 'r':
+            case 'r': // classify
                 classify(model, imagen);
                 break;
         }
